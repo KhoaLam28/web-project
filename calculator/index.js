204e7c76a731e9a769aa66f735f6e8b5d2a61a0f
Khoa Lam <khoadakao656@gmail.com>
2024-10-14 13:30:17 +0700

    First Commit

---
+++ calculator/index.js
@@ -0,0 +1,12 @@
+const display = document.getElementById("display");
+
+function appendtoDisplay(input) {
+    display.value += input
+
+}
+function clearDisplay() {
+
+}
+function calculate(){
+
+}
